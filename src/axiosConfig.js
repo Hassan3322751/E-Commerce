1d22f2f631da4aae7e622a06622badaf44d71581
Hassan3322751 <141483001+Hassan3322751@users.noreply.github.com>
2024-09-30 22:43:18 -0700

    ready for production

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
+  process.env.NODE_ENV !== 'production' ? process.env.REACT_APP_API_URL : process.env.REACT_APP_API_URL; 
